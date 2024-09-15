6309a72977ccd9c0ee7c442830c8944e52bee115
Esteban <135396733+Jesteban247@users.noreply.github.com>
2024-09-15 01:27:57 +0000

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,38 @@
 'use strict';
 
 // -------------------------------
-// Element Toggle Function
+// Utility Functions
 // -------------------------------
 
-const elementToggleFunc = function (elem) {
-  elem.classList.toggle("active");
-};
+const toggleElement = (elem) => elem.classList.toggle("active");
 
 // -------------------------------
-// Sidebar Variables and Functionality
+// Sidebar Functionality
 // -------------------------------
 
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-// Sidebar toggle functionality for mobile
-sidebarBtn.addEventListener("click", function () {
-  elementToggleFunc(sidebar);
+sidebarBtn.addEventListener("click", () => {
+  toggleElement(sidebar);
 });
 
 // -------------------------------
-// Page Navigation Variables and Functionality
+// Page Navigation Functionality
 // -------------------------------
 
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// Add event to all nav links
 navigationLinks.forEach((link) => {
-  link.addEventListener("click", function () {
-    const targetPage = this.innerHTML.toLowerCase();
+  link.addEventListener("click", () => {
+    const targetPage = link.textContent.toLowerCase();
 
-    pages.forEach((page, index) => {
+    pages.forEach((page) => {
       if (targetPage === page.dataset.page) {
         page.classList.add("active");
-        navigationLinks.forEach((navLink) => navLink.classList.remove("active"));
-        this.classList.add("active");
+        navigationLinks.forEach(navLink => navLink.classList.remove("active"));
+        link.classList.add("active");
         window.scrollTo(0, 0);
       } else {
         page.classList.remove("active");
@@ -46,59 +42,87 @@ navigationLinks.forEach((link) => {
 });
 
 // -------------------------------
-// Project Modal Variables and Functionality
+// Project Data and Modal Functionality
 // -------------------------------
 
-document.addEventListener('DOMContentLoaded', function() {
+const projects = {
+  'project-1': {
+    title: 'COVID-19 CT Scan Classification',
+    videoSrc: 'assets/Videos/Covid.mp4',
+    description: 'This project involves classifying COVID-19 CT scans using a CNN built on VGG16. The goal is to accurately distinguish between infected and non-infected cases based on the CT scan images.',
+    githubLink: 'https://github.com/Jesteban247/COVID-19-_CT_Scan_Classification'
+  },
+  'project-2': {
+    title: 'Tennis Video Analysis',
+    videoSrc: 'assets/Videos/Tenis.mov',
+    description: 'This project focuses on analyzing tennis match videos to extract key player movements and statistics. It utilizes computer vision techniques to track players and shots.',
+    githubLink: 'https://github.com/Jesteban247/Tennis-Video-Analysis'
+  },
+  'project-3': {
+    title: 'NEAT-based Car Simulation',
+    videoSrc: 'assets/Videos/Neat.mp4',
+    description: 'This project involves using the NeuroEvolution of Augmenting Topologies (NEAT) algorithm to simulate car behavior and optimize driving strategies in a virtual environment.',
+    githubLink: 'https://github.com/Jesteban247/NEAT-based-Car-Simulation'
+  },
+  'project-4': {
+    title: 'Fine-Tuning LLaMA-Factory with Math Dataset',
+    videoSrc: 'assets/Videos/LLaMA.mp4',
+    description: 'Guide on setting up and fine-tuning the Gemma-2-2B-Chat model using the Math Word Problems 200k dataset. Includes performance comparison before and after fine-tuning! 🌟',
+    githubLink: 'https://github.com/Jesteban247/Fine-Tuning-LLaMA-Factory-with-Math-Dataset'
+  },
+  'project-5': {
+    title: 'AI-Driven Gym and Nutrition Workflow',
+    videoSrc: 'assets/Videos/GymNutrition.mp4',
+    description: 'Leverages advanced AI technologies to optimize gym routines and dietary plans using CrewAI and IBM Watson’s language models.',
+    githubLink: 'https://github.com/Jesteban247/AI-Driven-Gym-and-Nutrition-Workflow-with-CrewAI-and-IBM-Watson'
+  },
+  'project-6': {
+    title: 'AI-Powered PDF & Image Processor',
+    videoSrc: 'assets/Videos/PDFImage.mp4',
+    description: 'Uses Google Gemini\'s Generative AI to answer questions from PDFs and generate content based on uploaded images. Features an easy-to-use Streamlit interface.',
+    githubLink: 'https://github.com/Jesteban247/AI-Powered-PDF-Image-Processor-GeminiAPI'
+  },
+  'project-7': {
+    title: 'RAG Project: Harry Potter Edition',
+    videoSrc: 'assets/Videos/HarryPotter.mp4',
+    description: 'Builds a Retrieval-Augmented Generation (RAG) pipeline using Harry Potter: The Complete Collection 🧹📜. Allows users to ask questions about the book and get responses based on the text.',
+    githubLink: 'https://github.com/Jesteban247/RAG-Project-Harry-Potter-Edition'
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
   const modal = document.getElementById('project-modal');
   const closeModalBtn = modal.querySelector('.close');
   const projectLinks = document.querySelectorAll('.project-link');
 
   projectLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
+    link.addEventListener('click', (event) => {
       event.preventDefault();
-      const projectId = this.getAttribute('data-project');
+      const projectId = link.getAttribute('data-project');
+      const { title, videoSrc, description, githubLink } = projects[projectId];
+
       const modalTitle = modal.querySelector('.modal-title');
       const modalVideo = modal.querySelector('.modal-video');
       const modalDescription = modal.querySelector('.modal-description');
       const modalGithubLink = modal.querySelector('.modal-github-link');
       
-      switch (projectId) {
-        case 'project-1':
-          modalTitle.textContent = 'COVID-19 CT Scan Classification';
-          modalVideo.querySelector('source').setAttribute('src', 'assets/Videos/Covid.mp4');
-          modalDescription.textContent = 'This project involves classifying COVID-19 CT scans using a CNN built on VGG16. The goal is to accurately distinguish between infected and non-infected cases based on the CT scan images.';
-          modalGithubLink.setAttribute('href', 'https://github.com/Jesteban247/COVID-19-_CT_Scan_Classification');
-          modalGithubLink.textContent = 'View on GitHub'; // Update link text
-          break;
-        case 'project-2':
-          modalTitle.textContent = 'Tennis Video Analysis';
-          modalVideo.querySelector('source').setAttribute('src', 'assets/Videos/Tenis.mov');
-          modalDescription.textContent = 'This project focuses on analyzing tennis match videos to extract key player movements and statistics. It utilizes computer vision techniques to track players and shots.';
-          modalGithubLink.setAttribute('href', 'https://github.com/Jesteban247/Tennis-Video-Analysis');
-          modalGithubLink.textContent = 'View on GitHub'; // Update link text
-          break;
-        case 'project-3':
-          modalTitle.textContent = 'NEAT-based Car Simulation';
-          modalVideo.querySelector('source').setAttribute('src', 'assets/Videos/Neat.mp4');
-          modalDescription.textContent = 'This project involves using the NeuroEvolution of Augmenting Topologies (NEAT) algorithm to simulate car behavior and optimize driving strategies in a virtual environment.';
-          modalGithubLink.setAttribute('href', 'https://github.com/Jesteban247/NEAT-based-Car-Simulation');
-          modalGithubLink.textContent = 'View on GitHub'; // Update link text
-          break;
-      }
+      modalTitle.textContent = title;
+      modalVideo.querySelector('source').setAttribute('src', videoSrc);
+      modalDescription.textContent = description;
+      modalGithubLink.setAttribute('href', githubLink);
+      modalGithubLink.textContent = 'View on GitHub'; // Update link text
 
       // Reload the video to ensure it plays from the start
       modalVideo.load();
-      
       modal.style.display = 'block';
     });
   });
 
-  closeModalBtn.addEventListener('click', function() {
+  closeModalBtn.addEventListener('click', () => {
     modal.style.display = 'none';
   });
 
-  window.addEventListener('click', function(event) {
+  window.addEventListener('click', (event) => {
     if (event.target === modal) {
       modal.style.display = 'none';
     }
